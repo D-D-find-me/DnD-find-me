e023f1a46e9ea0c009b612399ded46b4bcdd9c5e
unknown <jessop.lucas@gmail.com>
2020-12-18 15:55:43 -0700

    fixed little typos in schema

--- db/comments/get_comments.sql
+++ db/comments/get_comments.sql
@@ -1,5 +1,5 @@
 SELECT a.username, a.id, p.*, c.* FROM comments c
-JOIN posts p ON p.post_id = c.post_id
+JOIN posts p ON p.id = c.post_id
 JOIN adventurers a ON a.id = c.commentor_id
 WHERE c.post_id = $1
 ORDER BY c.comment_id DESC;
--- db/post/get_posts.sql
+++ db/post/get_posts.sql
@@ -1,3 +1,3 @@
 SELECT p.*, a.username, a.id FROM posts p
 JOIN adventurers a ON a.id =  p.adv_id
-ORDER BY p.post_id DESC;
+ORDER BY p.id DESC;
