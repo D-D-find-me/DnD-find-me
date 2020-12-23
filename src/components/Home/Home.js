93ea345ab62781cb46fd4571f6a7188b9f9f4d7e
Brian Demro <demrobrian@hotmail.com>
2020-12-23 12:26:43 -0600

    added post

--- src/components/Home/Home.js
+++ src/components/Home/Home.js
@@ -1,33 +1,53 @@
-import React, { useState, useEffect } from 'react';
-import { connect } from 'react-redux';
-import axios from 'axios';
+import React, { useState, useEffect } from "react";
+import { connect } from "react-redux";
+import axios from "axios";
+import { Link } from "react-router-dom";
 
 const Home = () => {
-const [posts, updatePosts] = useState([]);
+  const [posts, updatePosts] = useState([]);
 
-useEffect(() => {
+  useEffect(() => {
     const getPosts = async () => {
-        try {
-            const res = await axios.get('/api/post')
-            updatePosts(res.data)
-        } catch(err){
-            console.log('err on getposts func, frontend', err)
-        }
-    }
+      try {
+        const res = await axios.get("/api/post");
+        updatePosts(res.data);
+      } catch (err) {
+        console.log("err on getposts func, frontend", err);
+      }
+    };
     getPosts();
-}, [])
+  }, []);
 
-const mappedPosts = posts.map(post => {
-    return <div key={post.id}><h4>Title:</h4> {post.title} <br/><h6>by: {post.username}</h6><h4>Content:</h4> {post.content}</div>
-})
+  return (
+    <div className="Home">
+      <div className="home-header">
+        <h1>Adventure Board</h1>
+      </div>
+      <div className="posts">
+        {posts.length < 1 ? (
+          <img
+            src="https://media0.giphy.com/media/eIZZQ7npNjfsqizTwO/200w.webp?cid=ecf05e47bu1ve49khspqx0bmsciosj5c47omlleqbtgjatas&rid=200w.webp"
+            alt="loading gif"
+          />
+        ) : (
+          <ul style={{ listStyle: "none" }}>
+            {posts.map((post) => (
+              <li key={post.post_id}>
+                <Link
+                  style={{ textDecoration: "none", color: "black" }}
+                  to={`/posts/${post.post_id}`}
+                >
+                  <Post post={post} />
+                </Link>
+              </li>
+            ))}
+          </ul>
+        )}
+      </div>
+    </div>
+  );
+};
 
-    return (
-        <div>
-            {mappedPosts}
-        </div>
-    )
-}
-
-const mapStateToProps = state => state;
+const mapStateToProps = (state) => state;
 
 export default connect(mapStateToProps)(Home);
