c96317af32d90c41ee24211a455d89ef25d601db
Bradley Service <bradleykservice@gmail.com>
2020-12-28 17:33:43 -0600

    fixed some problems/reformatted thepost controller

--- server/controllers/postController.js
+++ server/controllers/postController.js
@@ -3,29 +3,57 @@ module.exports = {
     getPost: async (req, res) => {
         const db = req.app.get('db')
         const {id} = req.params
-        db.post.get_one_post(+id).then((posts) => res.status(200).send(posts)).catch(err => console.log(err))
+        try {
+            const post = await db.post.get_one_post(+id)
+            res.status(200).send(post)
+        } catch(err){
+            console.log('err on getPost in server', err)
+            res.sendStatus(502)
+        }
     },
     getPosts: async (req, res) => {
         const db = req.app.get('db')
-        db.post.get_posts().then((posts) => res.status(200).send(posts)).catch(err => console.log(err))
+        try {
+            const allPosts = await db.post.get_posts()
+            res.status(200).send(allPosts)
+        } catch(err){
+            console.log('err on getPosts in server', err)
+            res.sendStatus(503)
+        }
     },
     createPost: async (req, res) => {
         const db = req.app.get('db')
         const {title, content, zipcode} = req.body
         const {id} = req.session.user
-        await db.post.create_posts([title, content, zipcode, id])
-        res.sendStatus(200)
+        try {
+            const post = await db.post.create_posts([title, content, zipcode, id])
+            res.status(200).send(post)
+        } catch(err){
+            console.log('err on createPost serverside', err)
+            res.sendStatus(504)
+        }
     },
     updatePost: async (req, res) => {
         const db = req.app.get('db')
         const {id} = req.params
         const {title, description, zipcode} = req.body
-        await db.post.update_post([description, title, zipcode, +id]).then((posts) => res.status(200).send(posts)).catch(err => console.log(err))
+        try {
+            const newPost = await db.post.update_post([description, title, zipcode, +id])
+            res.status(200).send(newPost)
+        } catch(err){
+            console.log('err on updatePost server side', err)
+            res.sendStatus(505)
+        }
     },
     deletePost: async (req, res) => {
         const db = req.app.get('db')
         const {id} = req.params
-        await db.post.delete_get_post(+id)
-        res.sendStatus(200)
+        try {
+            const posts = await db.post.delete_post(+id)
+            res.status(200).send(posts)
+        } catch(err){
+            console.log('err on deletePost server side', err)
+            res.sendStatus(506)
+        }
     },
 }
