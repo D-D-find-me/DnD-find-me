2358dd0a97118238d3b3c4362e5d65ead0fdf363
Brian Demro <demrobrian@hotmail.com>
2020-12-30 12:28:21 -0600

    updating so that QueryClientProvider raps app

--- src/App.js
+++ src/App.js
@@ -1,20 +1,14 @@
 import Header from "./components/Header/Header";
 import routes from "./routes";
-import './App.css';
-import {useLocation} from "react-router-dom";
-import {QueryClient, QueryClientProvider} from "react-query"
-
-const queryClient = new QueryClient();
-
+import "./App.css";
+import { useLocation } from "react-router-dom";
 
 const App = () => {
   const location = useLocation();
   return (
     <div className="App">
-      <QueryClientProvider client={queryClient}>
       <Header />
       {routes}
-      </QueryClientProvider>
     </div>
   );
 };
