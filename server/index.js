d040f4396239bd6ab7700e1915d1ada41efad1c3
Brian Demro <demrobrian@hotmail.com>
2021-01-05 12:08:33 -0600

    game start functionality added

--- server/index.js
+++ server/index.js
@@ -1,13 +1,14 @@
-require('dotenv').config();
-const express = require('express');
-const massive = require('massive');
-const session = require('express-session');
+require("dotenv").config();
+const express = require("express");
+const massive = require("massive");
+const session = require("express-session");
 
-const {SERVER_PORT, CONNECTION_STRING, SESSION_SECRET} = process.env;
-const userCtrl = require('./controllers/userController');
-const cmntCtrl = require('./controllers/commentController');
-const postCtrl = require('./controllers/postController');
-const lociCtrl = require('./controllers/locationsController');
+const { SERVER_PORT, CONNECTION_STRING, SESSION_SECRET } = process.env;
+const userCtrl = require("./controllers/userController");
+const cmntCtrl = require("./controllers/commentController");
+const postCtrl = require("./controllers/postController");
+const textCtrl = require("./controllers/textController");
+const lociCtrl = require("./controllers/locationsController");
 
 const app = express();
 
@@ -16,55 +17,63 @@ const app = express();
 // app.use(express.static(`${__dirname}/../build`));
 app.use(express.json());
 app.use((req, res, next) => {
-    res.header('Access-Control-Allow-Origin', '*');
-    next();
-  });
-app.use(session({
+  res.header("Access-Control-Allow-Origin", "*");
+  next();
+});
+app.use(
+  session({
     resave: false,
     saveUninitialized: true,
     secret: SESSION_SECRET,
     cookie: {
-        maxAge: 1000 * 60 * 60 * 24 * 7
-    }
-}));
+      maxAge: 1000 * 60 * 60 * 24 * 7,
+    },
+  })
+);
 
 massive({
-    connectionString: CONNECTION_STRING,
-    ssl: {
-        rejectUnauthorized: false
-    }
-}).then(db => {
-    app.set('db', db)
-    console.log(`db is up, m'lord`)
-}).catch(err => console.log(err));
+  connectionString: CONNECTION_STRING,
+  ssl: {
+    rejectUnauthorized: false,
+  },
+})
+  .then((db) => {
+    app.set("db", db);
+    console.log(`db is up, m'lord`);
+  })
+  .catch((err) => console.log(err));
 
 // USER ENDPOINTS
-app.post('/api/login', userCtrl.login);
-app.post('/api/register', userCtrl.register);
-app.post('/api/logout', userCtrl.logout);
-app.put('/api/adventurer', userCtrl.editAdventurer);
-app.get('/api/adventurer', userCtrl.getAdventurer);
+app.post("/api/login", userCtrl.login);
+app.post("/api/register", userCtrl.register);
+app.post("/api/logout", userCtrl.logout);
+app.put("/api/adventurer", userCtrl.editAdventurer);
+app.get("/api/adventurer", userCtrl.getAdventurer);
 
 // COMMENT ENDPOINTS
-app.get('/api/comments/:post_id', cmntCtrl.getAllComments);
-app.post('/api/comments/:post_id', cmntCtrl.addComment);
-app.put('/api/comments/:commentId', cmntCtrl.updateComment);
-app.delete('/api/comments/:commentId', cmntCtrl.deleteComment);
-
+app.get("/api/comments/:post_id", cmntCtrl.getAllComments);
+app.post("/api/comments/:post_id", cmntCtrl.addComment);
+app.put("/api/comments/:commentId", cmntCtrl.updateComment);
+app.delete("/api/comments/:commentId", cmntCtrl.deleteComment);
 
 ///Post endpoints
-app.get('/api/post/:id', postCtrl.getPost)
-app.get('/api/post', postCtrl.getPosts)
-app.post('/api/post', postCtrl.createPost)
-app.put('/api/post/:id', postCtrl.updatePost)
-app.delete('/api/post/:id', postCtrl.deletePost)
+app.get("/api/post/:id", postCtrl.getPost);
+app.get("/api/post", postCtrl.getPosts);
+app.post("/api/post", postCtrl.createPost);
+app.put("/api/post/:id", postCtrl.updatePost);
+app.delete("/api/post/:id", postCtrl.deletePost);
 
-app.get('/api/locations', lociCtrl.getLocations)
-app.post('/api/locations', lociCtrl.createLocation)
-app.put('/api/locations/:id', lociCtrl.updateLocation)
-app.delete('/api/locations/:id', lociCtrl.deleteLocation)
+app.get("/api/locations", lociCtrl.getLocations);
+app.post("/api/locations", lociCtrl.createLocation);
+app.put("/api/locations/:id", lociCtrl.updateLocation);
+app.delete("/api/locations/:id", lociCtrl.deleteLocation);
+
+//Text Endpoint
+app.post("/api/text", textCtrl.sendSMS);
 // app.get('*', (req, res) => {
 //     res.sendFile(path.join(__dirname, '../build/index.html'))
 // });
 
-app.listen(SERVER_PORT, () => console.log(`server is running in ${SERVER_PORT}`));
+app.listen(SERVER_PORT, () =>
+  console.log(`server is running in ${SERVER_PORT}`)
+);
